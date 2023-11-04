561ce162e1e2312fe1dac28ab7fd7275c405c276
Eugenio Villaseñor Quiroz <evillaq@hotmail.com>
2023-11-04 12:11:18 -0600

    Survey class added

---
+++ app/src/main/java/com/vidixmx/nimblesurveys/data/model/Survey.kt
@@ -0,0 +1,25 @@
+package com.vidixmx.nimblesurveys.data.model
+
+
+import com.google.gson.annotations.SerializedName
+
+data class Survey(
+    @SerializedName("active_at")
+    val activeAt: String,
+    @SerializedName("cover_image_url")
+    val coverImageUrl: String,
+    @SerializedName("created_at")
+    val createdAt: String,
+    val description: String,
+    @SerializedName("inactive_at")
+    val inactiveAt: String,
+    @SerializedName("is_active")
+    val isActive: Boolean,
+    @SerializedName("survey_type")
+    val surveyType: String,
+    @SerializedName("thank_email_above_threshold")
+    val thankEmailAboveThreshold: String,
+    @SerializedName("thank_email_below_threshold")
+    val thankEmailBelowThreshold: String,
+    val title: String,
+)
