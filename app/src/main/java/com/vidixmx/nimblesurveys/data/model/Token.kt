51ed2bfc08b1f20758ac6afcb7e49fcc8dd7a1d5
Eugenio Villaseñor Quiroz <evillaq@hotmail.com>
2023-11-04 12:11:56 -0600

    Token class added

---
+++ app/src/main/java/com/vidixmx/nimblesurveys/data/model/Token.kt
@@ -0,0 +1,16 @@
+package com.vidixmx.nimblesurveys.data.model
+
+import com.google.gson.annotations.SerializedName
+
+data class Token(
+    @SerializedName("access_token")
+    val accessToken: String,
+    @SerializedName("created_at")
+    val createdAt: Int,
+    @SerializedName("expires_in")
+    val expiresIn: Int,
+    @SerializedName("refresh_token")
+    val refreshToken: String,
+    @SerializedName("token_type")
+    val tokenType: String,
+)
