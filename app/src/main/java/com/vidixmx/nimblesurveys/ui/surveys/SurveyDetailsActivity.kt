2d6c17279eb2c6172525f238cfbac7211f864f25
Eugenio Villaseñor Quiroz <evillaq@hotmail.com>
2023-11-06 13:22:52 -0600

    New layout added: activity_survey_details to show the survey details

---
+++ app/src/main/java/com/vidixmx/nimblesurveys/ui/surveys/SurveyDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.vidixmx.nimblesurveys.ui.surveys
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.vidixmx.nimblesurveys.R
+
+class SurveyDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_survey_details)
+    }
+}
